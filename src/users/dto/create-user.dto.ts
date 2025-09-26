ef1ba4e412e990455243e84265d67afd447497e9
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-09-26 22:45:05 +0100

    Update src/users/dto/create-user.dto.ts
    Co-authored-by: qodo-merge-pro[bot] <151058649+qodo-merge-pro[bot]@users.noreply.github.com>

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,27 @@
 import { $Enums } from 'generated/prisma';
+import { IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   matricNo: string;
+
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
   department: string;
+
+  @IsEnum($Enums.Role)
   role: $Enums.Role;
+
+  @IsString()
+  @MinLength(8)
   password: string;
 }
