62d2038f4a2f54f12983bf3741f2564b85f40118
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-09-26 20:21:13 +0000

    refactor: add Swagger decorators and type annotations to users controller
    - Add ApiTags decorator for Swagger documentation organization
    - Implement explicit return type annotations for all controller methods
    - Use UserDto for type-safe responses across all endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,30 +10,36 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Array<UserDto>> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<UserDto | null> {
     return this.usersService.findOne({
       id,
     });
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserDto> {
     return this.usersService.update(
       {
         id,
@@ -43,7 +49,7 @@ export class UsersController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<UserDto> {
     return this.usersService.remove({
       id,
     });
