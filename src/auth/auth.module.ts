b5f933eafec7048b1d5311565b51bf3621a39a4e
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-09-27 11:22:42 +0000

    refactor: fix module imports and exports
    - Remove duplicate imports in auth module
    - Export UsersService from users module for external use

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
-
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
