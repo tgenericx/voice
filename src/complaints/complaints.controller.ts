58d5ed4ac6bc8d9e53778025a2217474764c2828
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-10-24 07:58:41 +0000

    bootstrap complaint res

---
+++ src/complaints/complaints.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ComplaintsService } from './complaints.service';
+import { CreateComplaintDto } from './dto/create-complaint.dto';
+import { UpdateComplaintDto } from './dto/update-complaint.dto';
+import { CurrentUser } from 'src/utils/decorators';
+import { AuthenticatedUser } from 'src/auth/dto/auth.dto';
+
+@Controller('complaints')
+export class ComplaintsController {
+  constructor(private readonly complaintsService: ComplaintsService) {}
+
+  @Post()
+  create(
+    @Body() createComplaintDto: CreateComplaintDto,
+    @CurrentUser user: AuthenticatedUser,
+  ) {
+    return this.complaintsService.create(createComplaintDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.complaintsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.complaintsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateComplaintDto: UpdateComplaintDto,
+  ) {
+    return this.complaintsService.update(+id, updateComplaintDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.complaintsService.remove(+id);
+  }
+}
---
+++ src/complaints/complaints.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateComplaintDto } from './dto/create-complaint.dto';
+import { UpdateComplaintDto } from './dto/update-complaint.dto';
+
+@Injectable()
+export class ComplaintsService {
+  create(createComplaintDto: CreateComplaintDto) {
+    return 'This action adds a new complaint';
+  }
+
+  findAll() {
+    return `This action returns all complaints`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} complaint`;
+  }
+
+  update(id: number, updateComplaintDto: UpdateComplaintDto) {
+    return `This action updates a #${id} complaint`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} complaint`;
+  }
+}
