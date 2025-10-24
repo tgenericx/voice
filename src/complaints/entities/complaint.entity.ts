1323221b19da9983957106acaf3a81261187d140
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-10-24 14:28:46 +0000

    complaint dto

--- src/complaints/entities/complaint.entity.ts
+++ src/complaints/entities/complaint.entity.ts
@@ -1,1 +1,14 @@
-export class Complaint {}
+import { $Enums, Complaint as IComplaint } from 'generated/prisma';
+
+export class Complaint implements IComplaint {
+  id: string;
+  title: string;
+  category: $Enums.ComplaintCategory;
+  priority: $Enums.ComplaintPriority;
+  description: string;
+  status: $Enums.ComplaintStatus;
+  madeById: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+}
