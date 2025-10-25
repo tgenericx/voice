fb88c263d1c7e101826ed75bbca014a34dbc179d
root <root@localhost.localdomain>
2025-10-25 05:28:29 +0000

    feat: add attachments to complaints
    - Create `CreateAttachmentDto` with `fileName`, `fileUrl`, and `fileSize` fields
    - Add attachments array to `CreateComplaintDto` using `CreateAttachmentDto`

--- src/complaints/dto/create-complaint.dto.ts
+++ src/complaints/dto/create-complaint.dto.ts
@@ -1,8 +1,11 @@
 import { $Enums } from 'generated/prisma';
+import { CreateAttachmentDto } from 'src/attachments/dto/create-attachment.dto';
 
 export class CreateComplaintDto {
   title: string;
   category: $Enums.ComplaintCategory;
   priority: $Enums.ComplaintPriority;
   description: string;
+
+  attachments: Array<CreateAttachmentDto>;
 }
