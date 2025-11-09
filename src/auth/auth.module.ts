a0f782a9f84c701fde3e2be4bdf80549acc8ecb4
root <root@localhost.localdomain>
2025-11-09 16:34:25 +0000

    made cloudinary module available to auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { TokensModule } from 'src/tokens/tokens.module';
 import { JwtStrategy, RefreshTokenStrategy } from 'src/utils/strategies';
 import { JwtAuthGuard, RefreshTokenGuard } from 'src/utils/guards';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
   imports: [PrismaModule, TokensModule],
@@ -15,6 +16,7 @@ import { JwtAuthGuard, RefreshTokenGuard } from 'src/utils/guards';
     JwtAuthGuard,
     RefreshTokenStrategy,
     RefreshTokenGuard,
+    CloudinaryModule,
   ],
 })
 export class AuthModule {}
